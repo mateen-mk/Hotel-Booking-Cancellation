b2f6c623e304388040627772b0358c078d073a85
Mateen Khan <mateenmk2428@gmail.com>
2024-12-26 20:18:09 +0530

    Data Validation constants updated

--- src/core/constants/__init__.py
+++ src/core/constants/__init__.py
@@ -8,6 +8,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Common Constants
+DATA_VALIDATION_SPLIT_RATIO = 0.2
+SCHEMA_FILE_PATH = os.path.join("settings", "schema.yaml")
+
 # MySQL constants
 MYSQL_ENGINE_URL = os.getenv('MYSQL_ENGINE_URL')
 DATABASE_NAME = 'projects_db'
@@ -16,13 +20,11 @@ DATASET_NAME = 'hotel_booking'
 # Directory constants
 ARTIFACTS_DIR = 'artifacts'
 DATA_DIR = 'data'
+REPORTS_DIR = 'reports'
 
 # Logger constants (logger names)
 DATA_LOGGER = 'data'
 MODEL_LOGGER = 'model'
-
-# Common Constants
-SCHEMA_FILE_PATH = os.path.join("settings", "schema.yaml")
 
 # Data Ingestion constants
 RAW_DATA_DIR = os.path.join(from_root(), ARTIFACTS_DIR, DATA_DIR, 'raw')
@@ -30,5 +32,5 @@ DATA_INGESTION_RAW_FILE = 'raw.csv'
 DATA_INGESTION_DATA_FILE = 'data.csv'
 
 # Data Validation constants
-VALIDATION_DATA_DIR = os.path.join(from_root(), ARTIFACTS_DIR, DATA_DIR, 'validation')
+VALIDATION_DATA_DIR = os.path.join(from_root(), ARTIFACTS_DIR, REPORTS_DIR, 'validation')
 DATA_VALIDATION_REPORT = 'drift_report.yaml'
