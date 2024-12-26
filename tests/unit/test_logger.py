6bf47436ec311ec247d978dc222cd6344c644c00
Mateen Khan <mateenmk2428@gmail.com>
2024-12-26 18:35:52 +0530

    Data Ingestion performed

--- tests/unit/test_logger.py
+++ tests/unit/test_logger.py
@@ -1,5 +1,7 @@
 from src.core.logger import get_logger
 
-logger = get_logger('data')
+logger = get_logger('test')
 
 logger.info('test data logging')
+
+logger.info(("\n" + "_" * 100 + "\n\n") + "| | Started Data Ingestion Stage:\n" + ("- "*50))
