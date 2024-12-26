cf40ef6bca52ef11624e2a02bac3dfca16dde7ce
Mateen Khan <mateenmk2428@gmail.com>
2024-12-26 20:21:00 +0530

    Data Validation config entity added

--- src/core/entities/config_entity.py
+++ src/core/entities/config_entity.py
@@ -6,5 +6,11 @@ from src.core.constants import *
 # Data Ingestion Configuration
 @dataclass
 class DataIngestionConfig:
-    raw_file_path = os.path.join(RAW_DATA_DIR, DATA_INGESTION_RAW_FILE) 
-    data_file_path = os.path.join(RAW_DATA_DIR, DATA_INGESTION_DATA_FILE)
+    raw_file_path: str = os.path.join(RAW_DATA_DIR, DATA_INGESTION_RAW_FILE) 
+    data_file_path: str = os.path.join(RAW_DATA_DIR, DATA_INGESTION_DATA_FILE)
+
+
+# Data Validation Configuration
+@dataclass
+class DataValidationConfig:
+    validation_report_file_path: str = os.path.join(VALIDATION_DATA_DIR, DATA_VALIDATION_REPORT)
