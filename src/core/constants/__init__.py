146428754cc11169c717cff8816306ab641bafcd
Mateen Khan <mateenmk2428@gmail.com>
2024-12-26 11:08:33 +0530

    Data Ingestion constants added

--- src/core/constants/__init__.py
+++ src/core/constants/__init__.py
@@ -14,10 +14,18 @@ DATABASE_NAME = 'projects_db'
 DATASET_NAME = 'hotel_booking'
 
 # Directory constants
+ARTIFACTS_DIR = 'artifacts'
+
+# Data Directory constants
 DATA_DIR = 'data'
-RAW_DATA_DIR = os.path.join(from_root(), DATA_DIR, 'raw')
-INTERIM_DATA_DIR = os.path.join(from_root(), DATA_DIR, 'interim')
-PROCESSED_DATA_DIR = os.path.join(from_root(), DATA_DIR, 'processed')
+RAW_DATA_DIR = os.path.join(from_root(), ARTIFACTS_DIR, DATA_DIR, 'raw')
+INTERIM_DATA_DIR = os.path.join(from_root(), ARTIFACTS_DIR, DATA_DIR, 'interim')
+PROCESSED_DATA_DIR = os.path.join(from_root(), ARTIFACTS_DIR, DATA_DIR, 'processed')
+
+# Common Constants
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 # Data Ingestion constants
-RAW_DATA_FILE = 'data.csv'
+DATA_INGESTION_RAW_FILE = 'raw.csv'
+DATA_INGESTION_DATA_FILE = 'data.csv'
+DATA_INGESTION_LOGGER = 'Data_Ingestion'
