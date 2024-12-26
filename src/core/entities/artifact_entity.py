20935d59df4d9b565140cc10ee832c11976e772c
Mateen Khan <mateenmk2428@gmail.com>
2024-12-26 20:21:20 +0530

    Data Validation artifact entity added

--- src/core/entities/artifact_entity.py
+++ src/core/entities/artifact_entity.py
@@ -6,3 +6,11 @@ from dataclasses import dataclass
 @dataclass
 class DataIngestionArtifact:
     data_file_path: str
+
+
+# Data Validation Artifact
+@dataclass
+class DataValidationArtifact:
+    validation_status:bool
+    message: str
+    validation_report_file_path: str
