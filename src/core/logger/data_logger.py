455f714094eb8cb3b7f1cb6cefe33b691110fdd7
Mateen Khan <mateenmk2428@gmail.com>
2024-12-28 17:45:56 +0530

    src/core/logger/data_logger.py updated

---
+++ src/core/logger/data_logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+
+from from_root import from_root
+from datetime import datetime
+
+# Log constants
+LOGS_DIR = 'logs'
+LOGS_CAT = 'data_logs'
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+logs_dir = os.path.join(LOGS_DIR, LOGS_CAT)
+
+# Directory structure based on category
+logs_path = os.path.join(from_root(), logs_dir, LOG_FILE)
+print(f"Log file will be written to: {logs_path}")  # Debug statement
+os.makedirs(logs_dir, exist_ok=True)
+
+
+logging.basicConfig(
+    filename=logs_path,
+    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+)
