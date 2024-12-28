86b782b9a361e6cbe6dee6640388df863189f7f7
Mateen Khan <mateenmk2428@gmail.com>
2024-12-28 17:49:44 +0530

    src/pipelines/data_pipeline.py updated

--- src/pipelines/data_pipeline.py
+++ src/pipelines/data_pipeline.py
@@ -2,7 +2,7 @@
 
 import sys
 
-from src.core.logger import get_logger
+from core.logger.data_logger import logging
 from src.core.exception import HotelBookingException
 
 from src.core.entities.config_entity import (DataIngestionConfig,
@@ -13,7 +13,6 @@ from src.core.entities.artifact_entity import (DataIngestionArtifact,
 from src.data.data_ingestion import DataIngestion
 from src.data.data_validation import DataValidation
 
-from src.core.constants import DATA_LOGGER
 
 
 # Constructing a DataPipeline
@@ -24,11 +23,10 @@ class DataPipeline:
     """
 
     def __init__(self):
-        self.logger = get_logger(DATA_LOGGER)
 
-        self.logger.info("* "*50)
-        self.logger.info("- - - - - Started DataPipeline - - - - -")
-        self.logger.info("* "*50)
+        logging.info("* "*50)
+        logging.info("- - - - - Started DataPipeline - - - - -")
+        logging.info("* "*50)
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
 
@@ -38,23 +36,23 @@ class DataPipeline:
         This method of DataPipeline class is responsible for starting data ingestion component
         """
         try:
-            self.logger.info("_"*100)
-            self.logger.info("")
-            self.logger.info("! ! ! Entered start_data_ingestion method of DataPipeline Class:")
+            logging.info("_"*100)
+            logging.info("")
+            logging.info("! ! ! Entered start_data_ingestion method of DataPipeline Class:")
             
             data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
             data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-            self.logger.info("- "*50)
-            self.logger.info("- - - Data Ingested Successfully! - - -")
+            logging.info("- "*50)
+            logging.info("- - - Data Ingested Successfully! - - -")
 
-            self.logger.info("")
-            self.logger.info("! ! ! Exited the start_data_ingestion method of DataPipeline class:")
-            self.logger.info("_"*100)
+            logging.info("")
+            logging.info("! ! ! Exited the start_data_ingestion method of DataPipeline class:")
+            logging.info("_"*100)
 
             return data_ingestion_artifact
         
         except Exception as e:
-            self.logger.error(f"Error in start_data_ingestion: {str(e)}")
+            logging.error(f"Error in start_data_ingestion: {str(e)}")
             raise HotelBookingException(f"Error in start_data_ingestion: {str(e)}",sys) from e
         
 
@@ -63,24 +61,24 @@ class DataPipeline:
         This method of DataPipeline class is responsible for starting data validation component
         """
         try:
-            self.logger.info("_"*100)
-            self.logger.info("")
-            self.logger.info("! ! ! Entered start_data_validation method of DataPipeline Class:")
+            logging.info("_"*100)
+            logging.info("")
+            logging.info("! ! ! Entered start_data_validation method of DataPipeline Class:")
             
             data_validation = DataValidation(data_ingestion_artifact,
                                              self.data_validation_config)
             data_validation_artifact = data_validation.initiate_data_validation()
-            self.logger.info("- "*50)
-            self.logger.info("- - - Data Validated Successfully! - - -")
+            logging.info("- "*50)
+            logging.info("- - - Data Validated Successfully! - - -")
 
-            self.logger.info("")
-            self.logger.info("! ! ! Exited the start_data_validation method of DataPipeline class:")
-            self.logger.info("_"*100)
+            logging.info("")
+            logging.info("! ! ! Exited the start_data_validation method of DataPipeline class:")
+            logging.info("_"*100)
 
             return data_validation_artifact
         
         except Exception as e:
-            self.logger.error(f"Error in start_data_validation: {str(e)}")
+            logging.error(f"Error in start_data_validation: {str(e)}")
             raise HotelBookingException(f"Error in start_data_validation: {str(e)}",sys) from e
         
 
@@ -90,19 +88,19 @@ class DataPipeline:
         This method of DataPipeline class is responsible for running the entire data pipeline
         """
         try:
-            self.logger.info("_"*100)
-            self.logger.info("")
-            self.logger.info("$ Entered run_data_pipeline method of DataPipeline Class:")
+            logging.info("_"*100)
+            logging.info("")
+            logging.info("$ Entered run_data_pipeline method of DataPipeline Class:")
             
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
             
             # Further processing and data analysis can be added here
             
-            self.logger.info("")
-            self.logger.info("$ Exited the run_data_pipeline method of DataPipeline class:")
-            self.logger.info("_"*100)
+            logging.info("")
+            logging.info("$ Exited the run_data_pipeline method of DataPipeline class:")
+            logging.info("_"*100)
         
         except Exception as e:
-            self.logger.error(f"Error in run_data_pipeline: {str(e)}")
+            logging.error(f"Error in run_data_pipeline: {str(e)}")
             raise HotelBookingException(f"Error in run_data_pipeline: {str(e)}",sys) from e
