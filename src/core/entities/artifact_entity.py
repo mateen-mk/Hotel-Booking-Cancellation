5cf8b10ebaa2a32d959cefc16dbbf5b6d568c96d
Mateen Khan <mateenmk2428@gmail.com>
2024-12-29 21:53:29 +0530

    src/core/entities/artifact_entity.py updated

--- src/core/entities/artifact_entity.py
+++ src/core/entities/artifact_entity.py
@@ -14,3 +14,12 @@ class DataValidationArtifact:
     validation_status:bool
     message: str
     validation_report_file_path: str
+
+
+# Data Preprocessing Artifact
+@dataclass
+class DataPreprocessingArtifact:
+    train_data_file_path:str                # file path to preprocessed train data
+    test_data_file_path:str                 # file path to preprocessed test data 
+    validation_data_file_path:str           # file path to preprocessed validation data 
+    preprocessed_object_file_path:str       # file path to preprocessing.pkl
