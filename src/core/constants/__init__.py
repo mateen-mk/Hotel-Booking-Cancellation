b4bff3c0cb518eb073bda3b1e084a0695997e5bd
Mateen Khan <mateenmk2428@gmail.com>
2025-01-08 21:36:21 +0530

    src/core/constants data split constants updated

--- src/core/constants/__init__.py
+++ src/core/constants/__init__.py
@@ -8,8 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Common Constants
-TARGET_COLUMN = 'is_canceled'
-DATA_VALIDATION_SPLIT_RATIO = 0.2
+TARGET_COLUMN: str = 'is_canceled'
+VALIDATION_REPORT_SPLIT_RATIO: float = 0.2
+TEST_SET_SPLIT_RATIO: float = 0.3
+VALIDATION_SET_SPLIT_RATIO: float = 0.5
 SCHEMA_FILE_PATH = os.path.join("settings", "schema.yaml")
 MONTH_ORDER: list =['January', 'February', 'March', 'April', 'May', 'June', 
                     'July', 'August', 'September', 'October', 'November', 'December']
@@ -31,13 +33,16 @@ DATA_INGESTION_RAW_FILE: str = 'raw.csv'
 DATA_INGESTION_DATA_FILE: str = 'data.csv'
 
 # Data Validation constants
-VALIDATION_DATA_DIR = 'validation'
-DATA_VALIDATION_REPORT = 'drift_report.yaml'
+VALIDATION_DATA_DIR: str = 'validation'
+DATA_VALIDATION_REPORT: str = 'drift_report.yaml'
+
+# Data Split constants
+INTERIM_DATA_DIR: str = 'interim'
+DATA_SPLIT_TRAIN_FILE: str = "train.csv"
+DATA_SPLIT_TEST_FILE: str = "test.csv"
+DATA_SPLIT_VALIDATION_FILE: str = "val.csv"
 
 # Data Preprocessing constants
 PROCESSED_DATA_DIR: str = 'processed'
 PREPROCESSED_OBJECT_DIR: str = 'preprocessor'
-DATA_PREPROCESSING_TRAIN_FILE: str = "train.csv"
-DATA_PREPROCESSING_TEST_FILE: str = "test.csv"
-DATA_PREPROCESSING_VALIDATION_FILE: str = "val.csv"
 DATA_PREPROCESSING_OBJECT_FILE: str = 'preprocessor.pkl'
