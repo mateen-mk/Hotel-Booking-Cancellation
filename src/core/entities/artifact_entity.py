0c4b92f3252dd3a014a901291eb4c3bb75430fb3
Mateen Khan <mateenmk2428@gmail.com>
2025-01-08 21:41:44 +0530

    src/core/entities/artifact_entity.py data split artifacts added

--- src/core/entities/artifact_entity.py
+++ src/core/entities/artifact_entity.py
@@ -16,10 +16,18 @@ class DataValidationArtifact:
     validation_report_file_path: str
 
 
-# Data Preprocessing Artifact
+# Data Split Artifact
 @dataclass
-class DataPreprocessingArtifact:
-    train_data_file_path:str                # file path to preprocessed train data
-    test_data_file_path:str                 # file path to preprocessed test data 
-    validation_data_file_path:str           # file path to preprocessed validation data 
-    preprocessed_object_file_path:str       # file path to preprocessing.pkl
+class DataSplitArtifact:
+    train_data_file_path:str
+    test_data_file_path:str
+    validation_data_file_path:str 
+
+
+# # Data Preprocessing Artifact
+# @dataclass
+# class DataPreprocessingArtifact:
+#     train_data_file_path:str                # file path to preprocessed train data
+#     test_data_file_path:str                 # file path to preprocessed test data 
+#     validation_data_file_path:str           # file path to preprocessed validation data 
+#     preprocessed_object_file_path:str       # file path to preprocessing.pkl
