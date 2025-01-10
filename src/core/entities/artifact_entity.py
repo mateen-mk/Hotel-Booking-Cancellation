30920b6a8c0588ca04321473bce3d185c318c9c4
Mateen Khan <mateenmk2428@gmail.com>
2025-01-10 22:03:29 +0530

    src/core/entities/artifact_entity.py updated

--- src/core/entities/artifact_entity.py
+++ src/core/entities/artifact_entity.py
@@ -16,18 +16,16 @@ class DataValidationArtifact:
     validation_report_file_path: str
 
 
+# Data Preprocessing Artifact
+@dataclass
+class DataPreprocessingArtifact:
+    processed_data_file_path:str                # file path to preprocessed data
+    preprocessed_object_file_path:str           # file path to preprocessing.pkl
+
+
 # Data Split Artifact
 @dataclass
 class DataSplitArtifact:
     train_data_file_path:str
     test_data_file_path:str
     validation_data_file_path:str 
-
-
-# # Data Preprocessing Artifact
-# @dataclass
-# class DataPreprocessingArtifact:
-#     train_data_file_path:str                # file path to preprocessed train data
-#     test_data_file_path:str                 # file path to preprocessed test data 
-#     validation_data_file_path:str           # file path to preprocessed validation data 
-#     preprocessed_object_file_path:str       # file path to preprocessing.pkl
