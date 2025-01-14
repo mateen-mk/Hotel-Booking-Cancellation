16cf1893b2bffeb11c7c7693f9f807cfad1e7b7f
Mateen Khan <mateenmk2428@gmail.com>
2025-01-14 19:37:52 +0530

    src/data/data_preprocessing.py updated

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -14,10 +14,9 @@ from src.core.entities.artifact_entity import (DataPreprocessingArtifact,
                                                DataIngestionArtifact,
                                                DataValidationArtifact)
 
-from src.core.utils.object_utils import save_object
 from src.core.utils.yaml_utils import read_yaml
-from src.core.utils.data_utils import (read_data, 
-                                       save_data)
+from src.core.utils.object_utils import save_object
+from src.core.utils.data_utils import (read_data, save_data)
 
 from src.core.constants import (HOTEL_MAPPING,
                                 MONTH_ORDER,
