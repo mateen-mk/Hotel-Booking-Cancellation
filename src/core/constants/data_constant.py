30c057262f98c84117a9671c7d2b933154ca6644
Mateen Khan <mateenmk2428@gmail.com>
2025-01-15 20:41:55 +0530

    src/core/constants/data_constant.py added

---
+++ src/core/constants/data_constant.py
@@ -0,0 +1,17 @@
+# src/constants/data_constant.py is used to store data scripts related constant values 
+
+# Data Ingestion constants
+DATA_INGESTION_RAW_FILE: str = 'raw.csv'
+DATA_INGESTION_DATA_FILE: str = 'data.csv'
+
+# Data Validation constants
+DATA_VALIDATION_REPORT: str = 'drift_report.yaml'
+
+# Data Preprocessing constants
+DATA_PREPROCESSING_DATA_FILE: str = 'processed.csv'
+DATA_PREPROCESSING_OBJECT_FILE: str = 'preprocessor.pkl'
+
+# Data Split constants
+DATA_SPLIT_TRAIN_FILE: str = "train.csv"
+DATA_SPLIT_TEST_FILE: str = "test.csv"
+DATA_SPLIT_VALIDATION_FILE: str = "val.csv"
