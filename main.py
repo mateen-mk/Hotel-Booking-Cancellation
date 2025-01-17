c163f13fa7ca09da9913bf701522f8a81599d02c
Mateen Khan <mateenmk2428@gmail.com>
2025-01-17 18:52:49 +0530

    main.py updated

--- main.py
+++ main.py
@@ -4,22 +4,16 @@ import sys
 
 from src.core.exception import HotelBookingException
 
-from src.pipelines.data_pipeline import DataPipeline
-from src.pipelines.model_pipeline import ModelPipeline
-
-
-# create a data pipeline instance
-data_pipeline = DataPipeline()
-
-# create a model pipeline instance
-model_pipeline = ModelPipeline()
+from src.pipelines.run_pipe import run_pipe
 
 
 # run the pipeline
-try:
-    data_pipeline.run_data_pipeline()
-    model_pipeline.run_model_pipeline()
+if __name__ == "__main__":
 
-except HotelBookingException as e:
-    print(f"Error occured while running pipeline from main.py: {str(e)}")
-    raise HotelBookingException(f"Error occured while running pipeline from main.py: {str(e)}",sys) from e
+    try:
+        
+        run_pipe()
+
+    except HotelBookingException as e:
+        print(f"Error occured while running pipeline from main.py: {str(e)}")
+        raise HotelBookingException(f"Error occured while running pipeline from main.py: {str(e)}",sys) from e
