7b6c24c362dc7569e7790e5a008345039789b4ec
Mateen Khan <mateenmk2428@gmail.com>
2025-01-17 18:53:59 +0530

    src/pipelines/run_pipe.py updated

--- src/pipelines/run_pipe.py
+++ src/pipelines/run_pipe.py
@@ -6,46 +6,33 @@ from src.core.exception import HotelBookingException
 from src.pipelines.data_pipeline import DataPipeline
 from src.pipelines.model_pipeline import ModelPipeline
 
-def run_pipe():
+
+
+def run_pipe() -> None:
+    """
+    This method of DataPipeline class is responsible for running the entire data pipeline
+    """
     try:
         data_pipeline = DataPipeline()
         model_pipeline = ModelPipeline()
-
         logging.info("_"*100)
         logging.info("")
         logging.info("$ Entered run_data_pipeline method of DataPipeline Class:")
-
-        # start data pipeline
+        
+        # start the data pipeline
         data_ingestion_artifact = data_pipeline.start_data_ingestion()
         data_validation_artifact = data_pipeline.start_data_validation(data_ingestion_artifact)
         data_preprocessing_artifact = data_pipeline.start_data_preprocessing(data_ingestion_artifact, data_validation_artifact)
-
-        # start model pipeline
-        model_training_artifact = model_pipeline.start_model_trainer(data_preprocessing_artifact)
-            
+        data_split_artifact = data_pipeline.start_data_split(data_preprocessing_artifact)
+        
+        # start the model pipeline
+        model_trainer_artifact = model_pipeline.start_model_trainer(data_preprocessing_artifact=data_preprocessing_artifact, 
+                                                                    data_split_artifact=data_split_artifact)
+        
         logging.info("")
         logging.info("$ Exited the run_data_pipeline method of DataPipeline class:")
         logging.info("_"*100)
-        
+    
     except Exception as e:
-        raise HotelBookingException(f"Error in run_model_pipeline: {str(e)}",sys) from e
-
-
-    def run_data_pipeline(self) -> None:
-        """
-        This method of DataPipeline class is responsible for running the entire data pipeline
-        """
-        try:
-
-            
-            data_ingestion_artifact = self.start_data_ingestion()
-            data_validation_artifact = self.start_data_validation(data_ingestion_artifact)
-            data_preprocessing_artifact = self.start_data_preprocessing(data_ingestion_artifact, data_validation_artifact)
-            data_split_artifact = self.start_data_split(data_preprocessing_artifact)
-            
-            # Further processing and data analysis can be added here
-
-        
-        except Exception as e:
-            logging.error(f"Error in run_data_pipeline: {str(e)}")
-            raise HotelBookingException(f"Error in run_data_pipeline: {str(e)}",sys) from e
+        logging.error(f"Error in run_data_pipeline: {str(e)}")
+        raise HotelBookingException(f"Error in run_data_pipeline: {str(e)}",sys) from e
