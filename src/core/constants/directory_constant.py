f29b9c997a26714d697dd25d178b2f1470e157fa
Mateen Khan <mateenmk2428@gmail.com>
2025-01-30 14:26:19 +0530

    src/core/constants/directory_constant.py  updated

--- src/core/constants/directory_constant.py
+++ src/core/constants/directory_constant.py
@@ -15,6 +15,7 @@ SPLITTED_DATA_DIR: str = 'splitted'
 # Sub-Reports Directory constants
 VALIDATION_REPORT_DIR: str = 'validation'
 MODEL_METRICS_DIR: str = 'metrics'
+EVALUATION_REPORT_DIR: str = 'evaluation'
 
 # Sub-Objects Directory constants
 PREPROCESSED_OBJECT_DIR: str = 'preprocessor'
