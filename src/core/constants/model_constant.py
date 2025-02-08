dd81f0f22ea23fa6fb96a41ac128dc5df3675fff
Mateen Khan <mateenmk2428@gmail.com>
2025-02-08 14:59:11 +0530

    src/core/constant/model_constant.py updated

--- src/core/constants/model_constant.py
+++ src/core/constants/model_constant.py
@@ -1,6 +1,6 @@
 # Model Training related constants
 MODEL_TRAINER_MODEL_OBJECT_NAME: str = "model.pkl"
-MODEL_TRAINER_METRICS_FILE_NAME: str = "metrics.yaml"
+MODEL_TRAINER_BEST_MODEL_METRICS_NAME: str = "best_model.json"
 
 # Model Evaluation related constants
 MODEL_EVALUATION_REPORT_FILE_NAME: str = "report.json"
